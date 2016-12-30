d2cadc0c9ff309dcff78578559f39a19d3260042
elvis <elvis@lnmpy.com>
2016-12-30 22:27:32 +0800

    adapt for webpack2

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-require("style!css!less!./bootstrap-styles.loader!./bootstrap.config.js");
+require("style-loader!css-loader!less-loader!./bootstrap-styles.loader!./bootstrap.config.js");
 require("./bootstrap-scripts.loader!./bootstrap.config.js");
